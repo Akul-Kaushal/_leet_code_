5fde26a061ede97d4eb61ea4b0fb4f9b0694081b
Akul Kaushal <70272913+Akul-Kaushal@users.noreply.github.com>
2025-08-14 05:15:07 +0530

    Time: 21 ms (5.87%), Space: 40.9 MB (7.58%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -21,6 +21,7 @@ public:
         dfs(root->left, target-root->val, list, path);
         dfs(root->right, target-root->val, list, path);
 
+        path.pop_back();
     }
 
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
